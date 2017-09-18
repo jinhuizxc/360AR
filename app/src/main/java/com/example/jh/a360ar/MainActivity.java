38d9b8bc5c17bfbf6d15033ccf2567a683973fa3
JinHui <1004260403@qq.com>
2017-09-18 17:42:47 +0800

    toolbar

--- app/src/main/java/com/example/jh/a360ar/MainActivity.java
+++ app/src/main/java/com/example/jh/a360ar/MainActivity.java
@@ -2,14 +2,27 @@ package com.example.jh.a360ar;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView ivMine;
+    private TextView tvTitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        initView();
+        initListener();
 
+    }
+
+    private void initListener() {
+    }
+
+    private void initView() {
     }
 }
